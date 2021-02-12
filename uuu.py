f9b59ee096a69036a1f15af18940019ae3e229f8
renfich <yashach2005@gmail.com>
2021-02-12 23:14:53 +0200

    rocket

---
+++ uuu.py
@@ -0,0 +1,12 @@
+a = str(input("print the sentence"))
+def operations(a,operation):
+ return operation(a)
+def upper(a):
+    res = a.upper()
+    return res
+def lower(a):
+    res = a.lower()
+    return res
+
+
+
